1b52c0278bb5517e47e261f08ec63dd12951074e
ywhite1022 <y.white.1022@icloud.com>
2021-07-30 01:23:35 +0900

    update SP source

--- SP/js/schroll.js
+++ SP/js/schroll.js
@@ -1,13 +1,11 @@
-
 const scrollToArea = function (areaId)
 {
     let clientHeight = document.getElementById('header').clientHeight
     const areaLeft = document.getElementById(areaId).getBoundingClientRect().left;
     const areaTop = document.getElementById(areaId).getBoundingClientRect().top;
 
-    window.scrollTo({
-        left: areaLeft,
-        top: areaTop - clientHeight,
+    window.scrollBy({
+        top: areaTop,
         behavior: "smooth"
     });
 };
