5a380c9a9e3273427d0cea06f13e7ac952df3897
ywhite1022 <y.white.1022@icloud.com>
2021-07-30 01:59:13 +0900

    fix SP scroll

--- SP/js/schroll.js
+++ SP/js/schroll.js
@@ -1,11 +1,10 @@
 const scrollToArea = function (areaId)
 {
     let clientHeight = document.getElementById('header').clientHeight
-    const areaLeft = document.getElementById(areaId).getBoundingClientRect().left;
     const areaTop = document.getElementById(areaId).getBoundingClientRect().top;
 
     window.scrollBy({
-        top: areaTop,
+        top: areaTop - clientHeight,
         behavior: "smooth"
     });
 };
